b502141701dd2ac2744ff5416a546abb2e932dad
codewithheeren <heeren.tech@gmail.com>
2025-03-19 18:38:28 +0530

    Feign Client Implementation

--- spring-boot-microservices/5.currency-conversion-service_feign_client/src/main/java/com/codewithheeren/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ spring-boot-microservices/5.currency-conversion-service_feign_client/src/main/java/com/codewithheeren/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,7 +1,6 @@
 package com.codewithheeren.microservices.currencyconversionservice;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 /*
