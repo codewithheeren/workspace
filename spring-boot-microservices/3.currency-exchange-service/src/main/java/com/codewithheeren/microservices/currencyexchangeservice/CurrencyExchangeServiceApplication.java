696e43e474a22b1ea5c52d5f593efd8012c9d2ea
Heeren <87074236+codewithheeren@users.noreply.github.com>
2025-05-02 17:40:49 +0530

    Update CurrencyExchangeServiceApplication.java

--- spring-boot-microservices/3.currency-exchange-service/src/main/java/com/codewithheeren/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ spring-boot-microservices/3.currency-exchange-service/src/main/java/com/codewithheeren/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
+//Implementing h2-console database and exposing rest endpoint for currency exchange request.
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
