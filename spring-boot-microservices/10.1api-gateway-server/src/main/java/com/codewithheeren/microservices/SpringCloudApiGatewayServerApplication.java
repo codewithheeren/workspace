2f5da9e3c9a9e45676f491baef3143555119316e
codewithheeren <heeren.tech@gmail.com>
2025-05-20 20:35:51 +0530

    Api gateway implementation

---
+++ spring-boot-microservices/10.1api-gateway-server/src/main/java/com/codewithheeren/microservices/SpringCloudApiGatewayServerApplication.java
@@ -0,0 +1,18 @@
+package com.codewithheeren.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+//http://localhost:8100/currency-converter/from/USD/to/INR/quantity/5
+//http://localhost:8000//currency-exchange/from/USD/to/INR
+public class SpringCloudApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApiGatewayServerApplication.class, args);
+	}
+
+}
